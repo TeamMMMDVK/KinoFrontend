52e3ab9363178b1a2b8fa7ff0474f8182b113186
KristofferKay <37834268+KristofferKay@users.noreply.github.com>
2025-03-03 16:36:11 +0100

    Added html structure and init js

--- js/booking.js
+++ js/booking.js
@@ -1,1 +1,15 @@
-const bookingSlot = {};
+import { loadNavbar } from "../js/navbar.js";
+loadNavbar();
+
+// Dummy data - seat availability (true = available, false = unavailable)
+const seatAvailability = [
+    [true, true, false, true, true, true, true, false],
+
+];
+
+const seatsContainer = document.getElementById('seats-container');
+const selectedSeats = [];
+const movieSelect = document.getElementById('movie');
+const countElement = document.getElementById('count');
+const totalElement = document.getElementById('total');
+const bookButton = document.getElementById('book-button');
