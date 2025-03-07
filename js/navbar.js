977cd0024ff6fd2978c2702239e222b632763064
KristofferKay <37834268+KristofferKay@users.noreply.github.com>
2025-03-07 11:40:07 +0100

    init spa

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-export function loadNavbar() {
-  fetch("/components/navbar.html")
-    .then((response) => response.text())
-    .then((html) => {
-      document.getElementById("navbar-container").innerHTML = html;
-    })
-    .catch((error) => console.error("Error loading nav:", error));
-}
