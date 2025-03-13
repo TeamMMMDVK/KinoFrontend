78c351e1e473acb57fdb066a191358ad17268eeb
MetteMunch <meha0005@stud.kea.dk>
2025-03-13 14:21:55 +0100

    Opbygget html visning i bekræftelse (success)

--- src/js/success.js
+++ src/js/success.js
@@ -8,17 +8,43 @@ function loadCSS(filename) {
 
 loadCSS("css/success.css") // CSS fil indlæses når denne JS-fil kører
 
-const getSuccessObj = localStorage.getItem("successObj")
-const getContainer = document.getElementById("successContainer")
-
 
 function showReservation(){
-    let resDiv = document.createElement("div");
-    let resText = document.createElement("p")
-    resText.innerHTML = getSuccessObj
+    const getInfoLocalStorage = JSON.parse(localStorage.getItem("successObj"))
 
-    resDiv.appendChild(resText)
-    getContainer.appendChild(resDiv)
+    const resID = getInfoLocalStorage.reservationID //disse værdier kan vi hente direkte fra objektet
+    const totalPrice = getInfoLocalStorage.totalPrice
+    const resCustomerEmail = getInfoLocalStorage.customerEmail
+    const resCustomer = getInfoLocalStorage.customerName
+    const seatsInfo =[] //array til at gemme info om bookede sæder
+    let movieTitle = ""
+    let playTime = ""
+
+    //bookedSeats er et array i objektet, som vi skal hente info fra, hvis der er poster
+    if (getInfoLocalStorage.bookedSeats.length > 0){
+        const firstSeat = getInfoLocalStorage.bookedSeats[0] //Jeg bruger første objekt i arrayet til at få fat på title og tid
+        movieTitle = firstSeat.movieName
+        playTime = firstSeat.startTime
+
+        getInfoLocalStorage.bookedSeats.forEach( seat => { //Her iterere vi igennem tabellen for at få alle bookede sæder
+            const seatNumber = seat.seatNumber
+            const seatRow = seat.seatRow
+
+            seatsInfo.push({seatNumber,seatRow})
+        })
+    } else {
+        console.log("No booked seats found");
+    }
+
+    document.getElementById("resID").innerText = "ReservationID: " +resID
+    document.getElementById("resMovieTitle").innerText = "Movie: " +movieTitle
+    document.getElementById("resPlayTime").innerText = "StartTime: " +playTime
+
+    const seatDetails = seatsInfo.map(seat =>`Row: ${seat.seatRow}, Seat: ${seat.seatNumber}`).join(', ');
+    document.getElementById("resSeats").innerText = "Seats reserved: " +seatDetails
+    document.getElementById("resTotalPrice").innerText = "Total price DKK: " +totalPrice
+    document.getElementById("resCustomerEmail").innerText = "Email: " +resCustomerEmail
+
+
 }
-
 showReservation()
