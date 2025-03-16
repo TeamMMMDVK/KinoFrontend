abe0db23160e74519024e492b9f756317be48383
MetteMunch <meha0005@stud.kea.dk>
2025-03-16 13:20:36 +0100

    Lige tilføjet en if i routeren, så vi sikrer at vi starter med en # i url, så der ikke er blank side

--- src/router.js
+++ src/router.js
@@ -7,7 +7,12 @@ function initializeViewNavigation() {
 
 function handleViewChange() {
   let defaultView = "#home"; // default view
-
+  //For at undgå, at der ikke er # i url fra starten (og dermed blank side), tjekker vi med denne
+  //if statement og sætter til default view
+  if (!location.hash) {
+    location.hash = defaultView // Sætter hash til #home, hvis den mangler
+  }
+  //location.hash tjekker om der findes # i url og henter dette
   if (location.hash) {
     defaultView = location.hash; // extract the hash from the URL
     loadSectionScript(defaultView)
